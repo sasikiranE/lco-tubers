eab6777b37fe6d06781550cb739b2589c8bfbf6e
sasikiranE <sasikiranessamaraju@gmail.com>
2022-03-04 13:17:34 +0530

    register Slider model.

--- webpages/models.py
+++ webpages/models.py
@@ -1,3 +1,15 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+class Slider(models.Model):
+    headline = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    button_text = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to='media/slider/')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'{self.headline}'

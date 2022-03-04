e092c460a2575cb9a5f9e8773c3e9aff02d58ff4
sasikiranE <sasikiranessamaraju@gmail.com>
2022-03-04 11:18:43 +0530

    configure mySQL database.

--- tubers/settings.py
+++ tubers/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'tubers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lcotubers',
+        'USER': 'root',
+        'PASSWORD': 'sasikiran',
+        'HOST' : 'localhost',
     }
 }
 

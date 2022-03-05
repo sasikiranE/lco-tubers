9499457f778d70485a08fd0e43c46373645091ea
sasikiranE <sasikiranessamaraju@gmail.com>
2022-03-05 13:46:11 +0530

    configure inbuilt user authentication.

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login_user(request):
-    return HttpResponse('Login page')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+    
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            pass
+    
+    return render(request, 'accounts/login.html')
 
 
 def logout_user(request):
-    return HttpResponse('Logout page')
+    logout(request)
+    return redirect('login_user')
 
 
 def register_user(request):
-    return HttpResponse('register page')
+    if request.method == 'POST':
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password != confirm_password:
+            pass
+        elif User.objects.filter(username=username).exists():
+            pass
+        elif User.objects.filter(email=email).exists():
+            pass
+        else:
+            user = User.objects.create_user(username=username, first_name=firstname, last_name=lastname, email=email, password=password)
+            user.save()
+            return redirect('login_user')
+
+    return render(request, 'accounts/register.html')
 
 
 def dashboard(request):
-    return HttpResponse('Dashboard page')
+    return render(request, 'accounts/dashboard.html')
